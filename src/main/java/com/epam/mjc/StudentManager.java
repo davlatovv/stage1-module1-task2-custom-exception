dfeec7d46e4ce1229ee1171e03ec4c9c9e0f79b4
davlatovv <zafargithub@gmail.com>
2023-04-02 07:28:16 +0500

    Throw

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,17 +5,20 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+  public Student find(long studentID) throws IllegalAccessException {
+    if (Student.getValueOf(studentID) != null) {
+      return Student.getValueOf(studentID);
+    } else {
+      throw new IllegalAccessException("Could not find student with ID {" + studentID + "}");
+    }
   }
 
-  public static void main(String[] args) {
-    StudentManager manager = new StudentManager();
 
+  public static void main(String[] args) throws IllegalAccessException {
+    StudentManager manager = new StudentManager();
     for (int i = 0; i < IDs.length; i++) {
       Student student = manager.find(IDs[i]);
       System.out.println("Student name " + student.getName());
     }
-
   }
 }
